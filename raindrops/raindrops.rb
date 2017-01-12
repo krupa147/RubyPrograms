4b2335cb1169a7288c4bd63dc9c72770903dd313
krupa147 <krupasuthar147@gmail.com>
2017-01-12 16:29:30 +0530

    Raindrops program

---
+++ raindrops/raindrops.rb
@@ -0,0 +1,21 @@
+class Raindrops
+  def self.convert(num)
+		
+	  text = ""
+		if(num%3==0)
+      text += "Pling"
+    end
+    if(num%5==0)
+      text += "Plang"
+    end
+    if(num%7==0)
+      text += "Plong"
+    end
+    if text!=""
+      return text
+    else 
+     	return "#{num}"
+    end
+    
+	end
+end
