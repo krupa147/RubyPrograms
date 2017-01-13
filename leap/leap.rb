a3c6271faee1829ecad17948a2d6427096a90df2
krupa147 <krupasuthar147@gmail.com>
2017-01-13 10:59:42 +0530

    Update leap.rb

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,7 +1,7 @@
 class Year
   def self.leap?(year)
-  	if(year%4==0&&year%100!=0||year%400==0)
-  		return true
+    if(year%4==0&&year%100!=0||year%400==0)
+  	  return true
   	else
   	  return false
     end
