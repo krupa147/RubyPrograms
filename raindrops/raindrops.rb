ca36823c2db0ed2cef5ac2d0bfd6cac7abc82f71
krupa147 <krupasuthar147@gmail.com>
2017-01-16 12:16:21 +0530

    second commit raindrops

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,19 +1,13 @@
 class Raindrops  
   def self.convert(num)		
-	  text = ""
-		if(num%3==0)
-      text += "Pling"
-    end
-    if(num%5==0)
-      text += "Plang"
-    end
-    if(num%7==0)
-      text += "Plong"
-    end
+	text = ""
+	text += "Pling" if(num%3==0)
+    text += "Plang" if(num%5==0)
+    text += "Plong" if(num%7==0)
     if text!=""
       return text
     else 
-     	return "#{num}"
+      return "#{num}"
     end    
-	end
+  end
 end
