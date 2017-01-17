35ebe59b22fac5631285b454c206de2b414aebd0
krupa147 <krupasuthar147@gmail.com>
2017-01-17 10:02:48 +0530

    first commit acronym

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -2,9 +2,7 @@ class Acronym
   def self.abbreviate(arg)
   	text = arg.chars[0].upcase
     (1..arg.length-1).each do|i|
-      if arg.chars[i-1] == " " || arg.chars[i-1] == "-" || (arg.chars[i-1] =~ /[a-z]/ && arg.chars[i] =~ /[A-Z]/)
-        text += arg.chars[i].upcase
-      end
+      text += arg.chars[i].upcase if arg.chars[i-1] == " " || arg.chars[i-1] == "-" || (arg.chars[i-1] =~ /[a-z]/ && arg.chars[i] =~ /[A-Z]/)
     end
     text
   end
